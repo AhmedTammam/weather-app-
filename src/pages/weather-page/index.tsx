8e16c7368a4e5937cbd06c451cfa61ac03083cfb
Ahmed Tammam <tamam65@gmail.com>
2022-06-19 23:47:29 +0400

    initiate app with localStorageData

--- src/pages/weather-page/index.tsx
+++ src/pages/weather-page/index.tsx
@@ -1,11 +1,24 @@
+import { useEffect } from "react";
 import { Box } from "@chakra-ui/react";
+import { useLocalStorage } from "hooks/local-storage-hook";
+import { useRecoilState } from "recoil";
 
 import { Header } from "./componets/header";
 import { WeatherConditionsCards } from "./componets/weather-contions-cards";
+import { searchFormAtom } from "./store/atoms";
 
-export const WeatherPage = () => (
-  <Box>
-    <Header />
-    <WeatherConditionsCards />
-  </Box>
-);
+export const WeatherPage = () => {
+  const [{ citiesNames }, setFormState] = useRecoilState(searchFormAtom);
+  const [cities] = useLocalStorage("cities");
+
+  useEffect(() => {
+    const currentCities = cities ? [...cities, ...citiesNames] : citiesNames;
+    setFormState({ citiesNames: currentCities, hasError: false });
+  }, [cities]);
+  return (
+    <Box>
+      <Header />
+      <WeatherConditionsCards />
+    </Box>
+  );
+};
